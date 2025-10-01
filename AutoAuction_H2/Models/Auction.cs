a1fa9aaa890d2b5ff3a5cfe7c15bc8d0fe041147
Dvajhoej <Dvajhoej@gmail.com>
2025-10-01 10:31:28 +0200

    #

---
+++ AutoAuction_H2/Models/Auction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoAuction_H2.Models
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+        public Vehicle Vehicle { get; set; } = null!;
+        public User Seller { get; set; } = null!;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public decimal StartPrice { get; set; }
+        public decimal CurrentBid { get; set; }
+        public User? HighestBidder { get; set; }
+        public bool IsClosed => DateTime.Now >= EndTime;
+    }
+
+}
